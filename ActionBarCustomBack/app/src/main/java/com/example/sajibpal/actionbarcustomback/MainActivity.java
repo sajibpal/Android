79c33b71c2d43d7f8e28b8cb47a941b85e8d4df2
sajib pal <sajibpal20@gmail.com>
2021-02-06 01:07:50 +0600

    2nd commit

---
+++ ActionBarCustomBack/app/src/main/java/com/example/sajibpal/actionbarcustomback/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.sajibpal.actionbarcustomback;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setLogo(R.drawable.ic);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+    }
+}
